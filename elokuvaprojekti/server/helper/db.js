25c4f7ee082508a457fc1a2b2f489c4e5a68e2f1
Jesse <jesse.aittala@hotmail.com>
2025-10-10 18:52:13 +0300

    kirjautumiseen ja uloskirjautumiseen lisätty testit

--- elokuvaprojekti/server/helper/db.js
+++ elokuvaprojekti/server/helper/db.js
@@ -1,27 +1,37 @@
 import pkg from 'pg'
 import dotenv from 'dotenv'
+import fs from 'fs'
 
 // Käytettävän ympäristön määrittäminen
 const environment = process.env.NODE_ENV || 'development'
-// Lukee .env tiedoston muuttujat process.env:iin
-dotenv.config()
+
+if (environment === 'test' && fs.existsSync('.env.test')) {
+  dotenv.config({ path: '.env.test' })
+} else {
+  dotenv.config()
+}
+
 // Lukee portin ympäristömuuttujasta
 const port = process.env.PORT
 
 const { Pool } = pkg
+
 // Funktio tietokantayhteyden avaamiseen
 const openDb = () => {
-    const pool = new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: environment === "development" ? process.env.DB_NAME:
-        process.env.TEST_DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    })
+  const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database:
+      environment === 'development'
+        ? process.env.DB_NAME
+        : process.env.TEST_DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  })
 
     return pool
 }
+
 // Luo pool yhteyden
 const pool = openDb()
 
