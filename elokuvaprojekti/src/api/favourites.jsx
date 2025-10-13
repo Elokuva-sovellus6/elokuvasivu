309aa0575cdd46cce9a45add04eeae771174feb6
Jesse <jesse.aittala@hotmail.com>
2025-10-13 15:46:57 +0300

    kovakoodatut URL poistettu

--- elokuvaprojekti/src/api/favourites.jsx
+++ elokuvaprojekti/src/api/favourites.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/favourites';
+const BASE_URL = import.meta.env.VITE_API_URL + '/favourites';
 
 //Lisää elokuva suosikkeihin
 export const addFavourite = async (tmdbId) => {
